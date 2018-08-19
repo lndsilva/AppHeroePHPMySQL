6feba75406eb7860badba5f0d91642c91bc1e47b
Laercio Silva <lndsilva@hotmail.com>
2018-08-19 18:01:37 -0300

    alterações

--- app/src/main/java/br/senac/sp/l13/appheroephpmysql/Api.java
+++ app/src/main/java/br/senac/sp/l13/appheroephpmysql/Api.java
@@ -2,7 +2,7 @@ package br.senac.sp.l13.appheroephpmysql;
 
 public class Api {
 
-    private static final String ROOT_URL = "http://localhost/HeroApi/v1/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.1.39/HeroApi/v1/Api.php?apicall=";
 
     public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
     public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
